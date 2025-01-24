86e35ad196a81d1aea91f09bf5f477ce91640f05
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-01-24 19:17:50 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import DataProvider from "./context/DataProvider";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import DataProvider from './context/DataProvider';
 
-
-
-ReactDOM.render(<DataProvider><App /></DataProvider>, document.getElementById("root"));
+ReactDOM.render(
+  <DataProvider>
+    <App />
+  </DataProvider>,
+  document.getElementById('root')
+);
